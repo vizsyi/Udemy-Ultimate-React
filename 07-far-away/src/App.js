d881ed982e0c73730566d93f957ad186813305dd
Vizsy István <steve.visy@gmail.com>
2025-05-27 15:28:10 +0200

    #7: the structure of the web page

--- 07-far-away/src/App.js
+++ 07-far-away/src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
 
-function App() {
+function Logo(){
+  return <h1>🏝️ Far away 📸</h1>
+}
+
+function Form(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+      <select>
+        {Array.from({length: 20}, (_, i) => (i+1)).map(
+          num => (
+            <option value={num} key={num}>{num}</option>
+          )
+        )}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 }
 
-export default App;
+function Item({item}){
+  return (
+    <li>
+      <span style={item.packed ? {textDecoration: "line-through"} : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function PackingList(){
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map(item=><Item item={item} key={item.id} />)}
+      </ul>
+      <div>
+
+      </div>
+    </div>
+  );
+  
+  
+
+}
+
+function Stats(){
+  return (
+    <footer className="stats">
+      <em>🧳You have X items on your list, and you already packed X (X%) ✈️</em>
+    </footer>
+  )
+}
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+
+  );
+}
