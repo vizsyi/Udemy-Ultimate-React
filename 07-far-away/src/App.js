4e2a7d3996fe72cf4a99a8f03dfd522c0f1b6e9c
Vizsy István <steve.visy@gmail.com>
2025-05-29 00:01:14 +0200

    #7: sorting items

--- 07-far-away/src/App.js
+++ 07-far-away/src/App.js
@@ -61,12 +61,34 @@ function Item({item, onToggleItem, onDeleteItem}){
 }
 
 function PackingList({items, onDeleteItem, onToggleItem}){
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  switch(sortBy) {
+    case "description":
+      sortedItems = items.slice()
+        .sort((a, b) => a.description.localeCompare(b.description));
+      break;
+    case "packed":
+      sortedItems = items.slice()
+        .sort((a, b) => (Number(a.packed) - Number(b.packed)));
+      break;
+    default:
+      sortedItems = items;
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map(item => <Item item={item} key={item.id} onToggleItem={onToggleItem} onDeleteItem={onDeleteItem} />)}
+        {sortedItems.map(item => <Item item={item} key={item.id} onToggleItem={onToggleItem} onDeleteItem={onDeleteItem} />)}
       </ul>
-      <div>
+      <div className="actions">
+        <select value={sortBy} onChange={e => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
 
       </div>
     </div>
