d16641cd7318c4f151219d9f1810b5a57a8f7b3c
Vizsy István <steve.visy@gmail.com>
2025-05-30 16:16:07 +0200

    #10: refining the structure with component composition

---
+++ 10-usepopcorn/src/components/MovieList.js
@@ -0,0 +1,20 @@
+function Movie({ movie }) {
+  return <li>
+    <img src={movie.Poster} alt={`${movie.Title} poster`} />
+    <h3>{movie.Title}</h3>
+    <div>
+      <p>
+        <span>🗓</span>
+        <span>{movie.Year}</span>
+      </p>
+    </div>
+  </li>;
+}
+
+export function MovieList({ movies }) {
+  return <ul className="list">
+    {movies?.map((movie) => (
+      <Movie movie={movie} key={movie.imdbID} />
+    ))}
+  </ul>;
+}
